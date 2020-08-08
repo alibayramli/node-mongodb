b31e2af4f891b9c4fe29c7e2fa62356a06f0d2e5
“alibayramli” <bayramliali@tutanota.com>
2020-08-08 15:48:08 +0200

    Add mongodb setup

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const  Blog = require('./models/blog');
+const { result } = require('lodash');
 
 // express app
 const app = express();
 
-// listen for requests
-app.listen(3000);
+// connect to mongodb
+const dbURI = 'mongodb+srv://ali:ali123@demo-mongo.twzgh.mongodb.net/mongo-todos?retryWrites=true&w=majority';
+mongoose.connect(dbURI,{useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -27,6 +33,36 @@ app.use((req, res, next) => {
 });
 
 app.use(morgan('dev'));
+
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req,res) => {
+    const blog =  new Blog({
+      title:'new blog 2',
+      snippet: 'about my new blog',
+      body:'more about the blog'
+    });
+    blog.save()
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get('/single-blog', (req, res) => {
+  Blog.findById('5f2c23518b66c111c5bcdf5c')
+  .then((result) => {
+    res.send(result);
+  })
+  .catch((err) => console.log(err));
+})
 
 app.use((req, res, next) => {
   res.locals.path = req.path;
